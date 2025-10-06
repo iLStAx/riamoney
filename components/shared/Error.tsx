eda6f14fc36d5a06610b0959c3346c2f0331db79
Marco Salinas <salinas.marco.93@gmail.com>
2025-10-06 20:14:47 -0300

    feat: add shared components

---
+++ components/shared/Error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <p className="text-2xl font-bold text-center">Something was wrong, try again.</p>
+    </div>
+  )
+}
---
+++ components/shared/Loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen justify-center items-center ">
+      <p className="text-2xl font-bold text-center">Loading...</p>
+    </div>
+  )
+}
